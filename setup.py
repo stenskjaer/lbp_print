a74d347463386e3af7df3b51b2c6fc4aa0d0af1c
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2017-05-01 09:52:18 +0200

    Update the setup.py to actually be usable for installation

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
 setup(name='lbp_print',
-      version='0.0.1',
+      version='0.0.2',
+      packages=find_packages(),
+      scripts=['lbp_print.py'],
+      dependency_links=[
+          "git+https://github.com/lombardpress/lbppy.git@f272e44429fcbd3f19384471c9cfb3903100fb30#egg=lbppy",
+      ],
+      install_requires=[
+          'docopt==0.6.2',
+          'isodate==0.5.4',
+          'lbppy>=0.0.0',
+          'lxml==3.7.3',
+          'pyparsing==2.2.0',
+          'rdflib==4.2.2',
+          'SPARQLWrapper==1.8.0',
+          'untangle==1.1.0',
+      ],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+      ],
+
       description='Collection of utility scripts for handling LombardPress material.',
       url='https://github.com/stenskjaer/lbp_print',
       author='Michael Stenskjær Christensen',
       author_email='michael.stenskjaer@gmail.com',
       license='MIT',
-      zip_safe=False)
+)

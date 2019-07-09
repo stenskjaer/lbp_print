a7efb19e81537ec8f24cdb402ad15d4855abfe94
Michael Stenskjær Christensen <mschristensen@computas.com>
2019-07-09 13:00:21 +0200

    Move the default cache dir directly into the config file
    In this way it works in the same way when imported as module as from CLI.

--- lbp_print/config.py
+++ lbp_print/config.py
@@ -4,5 +4,5 @@ To change any settings, simply overwrite then after import."""
 
 import os
 
-cache_dir = None
+cache_dir = os.path.join(os.path.expanduser("~"), ".lbp_cache")
 module_dir = os.path.dirname(__file__)

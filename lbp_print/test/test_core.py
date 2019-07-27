c06605fc310a1a9cbe6c13980c8651d29ac6f320
Michael Stenskjær Christensen <mschristensen@computas.com>
2019-07-27 15:21:00 +0200

    Move tests into single file for core lib

---
+++ lbp_print/test/test_core.py
@@ -0,0 +1,29 @@
+import os
+
+from lbp_print.core import RemoteResource, UrlResource
+
+
+class TestUrlResource:
+
+    url = "https://raw.githubusercontent.com/scta-texts/da-49/master/da-49-l1q1/da-49-l1q1.xml"
+    res = UrlResource(url)
+
+    def test_url_resource_identified(self):
+        assert self.res.url == self.url
+
+    def test_successfull_download_to_file(self):
+        assert os.path.exists(self.res.filename)
+
+
+class TestRemoteResource:
+
+    trans = RemoteResource("da-49-l1q1")
+
+    def test_remote_transcription_object(self):
+        assert (
+            self.trans.transcription_object.to_s()
+            == "http://scta.info/resource/da-49-l1q1/critical/transcription"
+        )
+
+    def test_remote_file_download(self):
+        assert self.trans.file is not None

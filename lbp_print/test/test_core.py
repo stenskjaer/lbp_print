6c20cef9226a5f795d318c128f160b8aad6d3e26
Michael Stenskjær Christensen <mschristensen@computas.com>
2019-07-28 09:47:52 +0200

    Setup basic testing of cache

--- lbp_print/test/test_core.py
+++ lbp_print/test/test_core.py
@@ -1,6 +1,10 @@
 import os
+import shutil
 
-from lbp_print.core import RemoteResource, UrlResource
+import pytest
+
+from lbp_print.core import LocalResource, RemoteResource, UrlResource, Tex
+from lbp_print import config
 
 
 class TestUrlResource:
@@ -41,3 +45,17 @@ class TestRemoteResource:
     def test_resource_id_exists(self):
         assert type(self.res.id) == str
 
+
+class TestCache:
+    path = os.path.join(config.module_dir, "test", "assets", "da-49-l1q1.xml")
+    res = LocalResource(path)
+
+    @pytest.fixture
+    def cache_settings(self):
+        config.cache_dir = os.path.join(os.path.curdir, "cache")
+        yield
+        shutil.rmtree(config.cache_dir)
+
+    def test_store_in_cache_after_processing(self, cache_settings):
+        Tex(self.res).process(output_format="tex")
+        assert os.path.isfile(os.path.join(config.cache_dir, self.res.digest + ".tex"))

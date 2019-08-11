5d4e2426c60adc791311268dbedddcac215efe03
Michael Stenskjær Christensen <mschristensen@computas.com>
2019-08-11 13:58:10 +0200

    Put log level in the config too

--- lbp_print/config.py
+++ lbp_print/config.py
@@ -2,7 +2,9 @@
 
 To change any settings, simply overwrite then after import."""
 
+import logging
 import os
 
 cache_dir = os.path.join(os.path.expanduser("~"), ".lbp_cache")
 module_dir = os.path.dirname(__file__)
+log_level = logging.INFO
